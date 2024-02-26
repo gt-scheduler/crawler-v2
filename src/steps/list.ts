5ac2837aaff9d13fac995f8e8532d6f91e598b1c
Yatharth Bhargava <52095139+yatharth-b@users.noreply.github.com>
2024-02-26 12:19:48 +0530

    Finalized Schedule Alert (#31)
    * new term type
    
    * Tian/finalized (#30)
    
    * implemented finalized term crawl
    
    * fix issue with output format into json
    
    * fix json output format
    
    * output format
    
    ---------
    
    Co-authored-by: tsun90 <tian.sun@outlook.com>
    
    ---------
    
    Co-authored-by: tsun90 <tian.sun@outlook.com>

--- src/steps/list.ts
+++ src/steps/list.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { getIntConfig } from "../utils";
+import { Term } from "../types";
 
 const NUM_TERMS = getIntConfig("NUM_TERMS") ?? 2;
 
@@ -8,17 +9,23 @@ export type TermData = {
   description: string;
 };
 
-export async function list(): Promise<string[]> {
+export async function list(): Promise<[string[], string[]]> {
   const queryNum = 3 * NUM_TERMS + 10;
 
   const response = await axios.post<TermData[]>(
     `https://registration.banner.gatech.edu/StudentRegistrationSsb/ssb/courseSearch/getTerms?searchTerm=&offset=1&max=${queryNum}`
   );
-  const { data } = response;
+  const responseFinalized = await axios.post<TermData[]>(
+    `https://registration.banner.gatech.edu/StudentRegistrationSsb/ssb/classSearch/getTerms?searchTerm=&offset=1&max=${queryNum}`
+  );
 
-  const terms = data.map((term) => term.code);
-  return terms.filter((term) => {
+  const terms = response.data.map((term) => term.code);
+  const termsFinalized = responseFinalized.data.map((term) => term.code);
+
+  const results = terms.filter((term) => {
     const month = Number(term.slice(4));
     return month >= 1 && month <= 12;
   });
+
+  return [results, termsFinalized];
 }
